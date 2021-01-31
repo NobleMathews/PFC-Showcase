7b997025f8289fa967bd384c43fa2db005231361
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-02-01 00:09:45 +0530

    Clean Up #1
    Cleaned up files
    Testing Masonry

--- client-gallery/src/components/Gallery/index.js
+++ client-gallery/src/components/Gallery/index.js
@@ -1,73 +1,72 @@
 import React, {useState, useEffect} from 'react'
-import {config} from '../../config';
 import {getAlbumsArrObj} from '../helpers/await_all';
 import styled from 'styled-components';
 import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
-import { getAlbumID, getAlbumName, getImgSrc } from '../helpers/album_metadata';
-import { values } from 'lodash';
+import { getAlbumName, getImgSrc } from '../helpers/album_metadata';
 import { useParams } from 'react-router-dom';
-import { ParallaxProvider, Parallax, ParallaxBanner } from 'react-scroll-parallax';
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 var _ = require('lodash');
 
 const Gallery = () => {
-    const [images, setImages] = useState([])
 
+    const [images, setImages] = useState([])
     const {id} = useParams();
     const pagename = getAlbumName(id);
 
-    // Querying everything parallely to cache on homepage
     useEffect(() => {
         (async function(){
-            // const values = config.albumIDs;
             const values = [id];
             const res = getAlbumsArrObj(values);
             const result = await res;
             const images = _(result).filter(album => album.status === "fulfilled").map('value').value();
             setImages(images);  
         })();
-    }, [])
+    }, [id])
 
     return (
-        <>
-        {/* Just a placholder will need to come up with a good overall theme / style language */}
+        <GalleryMain>
         <ParallaxProvider>
-            
-            
             <Jumbotron className="sticky">
                 <div className="container">
                 <h1 className="display-2"><b>{pagename}</b></h1>
                 </div>
             </Jumbotron>
-            
             <Parallax y={[0,0]}>
-            <div className="cont">
-            {
-                <img src={getImgSrc("https://lh3.googleusercontent.com/BXvyRjK2pw-skWDRQgEtxAsxbp2KKSTVDpvd3WRlqWO0dnBb31KIc87zGkcnGztRk8xYnMmVOQAk9LUgRaFif2o98tv4GgfBfLUfFYFV3RSXiLHJnqowP2s-oO-pnq-gfL73IjM6Qg",true)} alt={"Testing out variable width"} />
-            }
-            {images.map(({name,data}) => (
-                <>
-                <h1>{pagename}</h1>
-                <ResponsiveMasonry
-                    columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
-                >
-                    <Masonry>
-                    {data.map(image=>(
-                    <p>{image}</p>
-                    ))}
-                    </Masonry>
-                </ResponsiveMasonry>
-                
-                </>
-            ))}
-            </div>
-
-        {/* </ParallaxBanner> */}
-        </Parallax>
+                <GalleryContainer>
+                {images.map(({data}) => (
+                    <ResponsiveMasonry
+                        columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                    >
+                        <Masonry>
+                        <img alt="Testing masonry packing" style={{width: "100%", display: "block"}} src={"https://picsum.photos/200/300"} />
+                        {data.map((image,i)=>(
+                            <img key={i} alt={`${pagename}#${i}`} style={{width: "100%", display: "block"}} src={getImgSrc(image,true)} />
+                        ))}
+                        </Masonry>
+                    </ResponsiveMasonry>
+                ))}
+                </GalleryContainer>
+            </Parallax>
         </ParallaxProvider>
-        </>
+        </GalleryMain>
     )
 }
 
+const GalleryMain = styled.div`
+    .sticky {
+        position: sticky;
+        top: 0;
+        margin: 0;
+        padding: 50px;
+        height: 240px;
+    }
+`
+
+const GalleryContainer = styled.div`
+    background-color: #fff;
+    padding: 20px;
+    margin: 0;
+`
 const Jumbotron = styled.div`
     background: 
     linear-gradient(
