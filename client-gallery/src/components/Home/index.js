151b6f656f9b5e4dc78db85afe0d5a05f40ea340
Debeshee <debesheedas@gmail.com>
2021-02-01 00:57:13 +0530

    homepage grid v1

--- client-gallery/src/components/Home/index.js
+++ client-gallery/src/components/Home/index.js
@@ -3,6 +3,9 @@ import {config} from '../../config';
 import {getAlbumsArrObj} from '../helpers/await_all';
 import styled from 'styled-components';
 import {getAlbumPreview} from '../helpers/album_metadata';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 // import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 // var _ = require('lodash');
 
@@ -25,22 +28,144 @@ const Home = () => {
     return (
         <>
         {/* Just a placholder will need to come up with a good overall theme / style language */}
+       
         <Jumbotron className="jumbotron jumbotron-fluid">
             <div className="container">
             <h1 className="display-2"><b>PFC Showcase</b></h1>
             {/* <p className="lead">TagLine - Modify to a fixed dark image for it to look good </p> */}
             </div>
         </Jumbotron>
-        {Object.keys(config.albumIDs).map((name)=> (
-            <>
-            {/* just for testing need to use a proper component */}
-            <a style={{display:"block"}} href={`/gallery/${config.albumIDs[name]}`}>{name}</a>
-            <img src={getAlbumPreview(name)} alt={"Placeholder preview"} />
+        <ContainerCustom>
+        <Container fluid={true} >
+          <Row fluid={true} xs={1} md={2} lg={3} xl={4} noGutters={false}>
+              
+            {Object.keys(config.albumIDs).map((name)=> (
+            <>            
+                <div class="card-deck"> 
+                 
+                <div fluid={true} class="card">
+                <a class="entireCard" style={{display:"block"}} href={`/gallery/${config.albumIDs[name]}`}> 
+                <div class="card-body">
+                <img class="card-img-top" src={getAlbumPreview(name)} alt={"Placeholder preview"} />
+                <h5 class="title">{name}</h5>
+                {/* <a class="title" style={{display:"block"}} href={`/gallery/${config.albumIDs[name]}`}>{name}</a> */}
+                </div>
+                </a>
+                </div>
+               
+                </div>
+                
             </>
-        ))}
-        </>
+            ))}
+    
+          </Row>
+        </Container>
+        </ContainerCustom>
+    </>
     )
 }
+/*Polaroid Version*/
+
+const ContainerCustom = styled.div`
+   
+    margin: 15px 15px 60px 30px;
+    border-radius:10px; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    
+    .card-body{
+        padding:0px 5px 0px 5px;
+    }
+    .card-deck{
+        
+        padding:5px 8px 5px 8px;
+        border-radius:0px; 
+        background-color: transparent;
+    }
+    .card{
+        margin: auto 16px auto 16px;
+        padding:0px 0px 0px 0px;
+        background-color: white;
+        border-radius:0px;
+    }
+    .card-img-top{
+        padding:20px 0px 15px 0px;
+        border-radius:0px;
+    }
+    .entireCard{
+        padding:10px 8px 20px 8px;
+        background-color: transparent;
+        color: black;
+        text-align: center;
+        margin:0px 0px 0px 0px;
+        
+    }
+    .entireCard:hover{
+        box-shadow: 10px 10px 5px grey;
+        background-color: linear-gradient(to right, rgba(255,0,0,0), rgba(255,0,0,1));;
+        color: black;
+        text-align: center;
+        margin:0px 0px 0px 0px;
+        text-decoration: none;
+    }
+    .title{
+        font-family: 'Permanent Marker', cursive;
+        text-decoration: none;
+    }
+`
+ 
+/*Non-Polaroid Version*/
+/* const ContainerCustom = styled.div`
+    margin: 15px 15px 60px 30px;
+    border-radius:10px; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    
+    .card-body{
+        padding:0px 0px 0px 0px;
+        margin:0px 0px 0px 0px;
+    }
+    .card-deck{
+        padding:5px 8px 5px 8px;
+        border-radius:0px; 
+        background-color: transparent;
+    }
+    .card{
+        border-style: none;
+        margin: auto 16px auto 16px;
+        padding:0px 0px 0px 0px;
+        background-color: white;
+        border-radius:0px;
+    }
+    .card-img-top{
+        padding:0px 0px 0px 0px;
+        margin:0px 0px 0px 0px;
+        border-radius:5px;
+    }
+    .entireCard{
+        padding:0px 0px 0px 0px;
+        background-color: transparent;
+        border-radius: 5px;
+        color: black;
+        text-align: center;
+        margin:0px 0px 0px 0px;
+    }
+    .entireCard:hover{
+        box-shadow: 10px 10px 5px grey;
+        margin:0px 0px 0px 0px;
+        text-decoration: none;
+    }
+    .title{
+        text-transform: uppercase;
+        font-family: Arial, Helvetica, sans-serif;
+        text-decoration: none;
+        padding: 15px 0px 5px 0px
+    }
+` */
 
 const Jumbotron = styled.div`
     margin:15px 15px 30px 15px;
@@ -56,5 +181,4 @@ const Jumbotron = styled.div`
     color:white !important;
     height:340px;
 `
-
 export default Home;
