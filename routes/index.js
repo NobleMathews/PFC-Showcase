a3a2acb0d59f78661f35c95148ef8e29ac5f03bf
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-02-06 16:29:41 +0530

    Working test

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 // router.get('/api', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
 
 /* GET React App */
-router.get(['/home', '/home/*'], function(req, res, next) {
+router.get(['/', '/*'], function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public', 'app.html'));
 });
 
