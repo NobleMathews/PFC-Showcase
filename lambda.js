95608febd0299dbb4e7b01b62272600238128aa5
Noble Mathews <55821103+NobleMathews@users.noreply.github.com>
2021-02-06 17:03:14 +0530

    test2

--- lambda.js
+++ lambda.js
@@ -10,5 +10,6 @@ const binaryMimeTypes = [
 	'image/png',
 	'image/svg+xml'
 ]
+// "build": "cd client-gallery && yarn build && cd .. && xcopy /e/h/y/q \"./client-gallery/build\" \"./public\" && move public\\index.html public\\app.html",
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
